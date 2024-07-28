12baaeca411e33844e51097ef12ea941bfb69bc5
Dzianis <2119348+dzianisv@users.noreply.github.com>
2024-07-28 22:17:24 +0300

    Do not import webdriver_manager

--- poept/poept.py
+++ poept/poept.py
@@ -5,7 +5,7 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
